89a949d21b82ab7a8b621d9d94ffdec79292d8b3
rida-hdj <rida.hdj@users.noreply.github.com>
2026-06-22 00:14:53 +0100

    add yes/no

---
+++ target/debug/deps/ncli-1979c14af1ee4ee5.d
@@ -0,0 +1,5 @@
+/home/rida/dev/programming-langs/rust/ncli/target/debug/deps/ncli-1979c14af1ee4ee5.d: src/main.rs
+
+/home/rida/dev/programming-langs/rust/ncli/target/debug/deps/libncli-1979c14af1ee4ee5.rmeta: src/main.rs
+
+src/main.rs:
---
+++ target/debug/deps/ncli-fb81e918e42a7ee7.d
@@ -0,0 +1,5 @@
+/home/rida/dev/programming-langs/rust/ncli/target/debug/deps/ncli-fb81e918e42a7ee7.d: src/main.rs
+
+/home/rida/dev/programming-langs/rust/ncli/target/debug/deps/libncli-fb81e918e42a7ee7.rmeta: src/main.rs
+
+src/main.rs:
