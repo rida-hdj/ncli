39d637f0ff096fe686909a184001c033b0000672
rida-hdj <rida.hdj@users.noreply.github.com>
2026-06-23 21:10:16 +0100

    add gitignore

--- target/release/build/owo-colors-f8cfbc7742f92532/build_script_build-f8cfbc7742f92532.d
+++
@@ -1,5 +0,0 @@
-/home/rida/dev/programming-langs/rust/ncli/target/release/build/owo-colors-f8cfbc7742f92532/build_script_build-f8cfbc7742f92532.d: /home/rida/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/owo-colors-4.3.0/build.rs
-
-/home/rida/dev/programming-langs/rust/ncli/target/release/build/owo-colors-f8cfbc7742f92532/build_script_build-f8cfbc7742f92532: /home/rida/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/owo-colors-4.3.0/build.rs
-
-/home/rida/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/owo-colors-4.3.0/build.rs:
